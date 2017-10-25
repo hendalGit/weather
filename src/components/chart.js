9b2be092793650ea795ef4f9750d0f9554b4d351
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-10-26 00:34:00 +0200

    Some style modifications

--- src/components/chart.js
+++ src/components/chart.js
@@ -7,12 +7,22 @@ function average(data) {
   return _.round(_.sum(data)/data.length);
 }
 
+function max(data) {
+  return _.round(data.reduce((x, y) => x > y ? x : y, data[0]));
+}
+
+function min(data) {
+  return _.round(data.reduce((x, y) => x < y ? x : y, data[0]));
+}
+
 export default ({data, color, units}) => (
   <div>
     <Sparklines width={180} height={120} data={data}>
       <SparklinesLine color={color} />
       <SparklinesReferenceLine type="avg" />
     </Sparklines>
-    <div>Average : {average(data)} {units}</div>
+    <div className="chart"><div className="top average">Average : {average(data)} {units}</div></div>
+    <div className="chart"><div className="top max">{max(data)} {units}</div></div>
+    <div className="chart"><div className="min">{min(data)} {units}</div></div>
   </div>
 );
